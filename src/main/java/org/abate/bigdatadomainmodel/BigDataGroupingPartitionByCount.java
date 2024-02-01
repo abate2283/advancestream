4565f834efaa919ab55aacd5843a60c7d5604b61
abate2283 <alfredbate@yahoo.co.uk>
2024-02-01 13:03:25 -0600

    added files with changes

--- src/main/java/org/abate/bigdatadomainmodel/BigDataGroupingPartitionByCount.java
+++ src/main/java/org/abate/bigdatadomainmodel/BigDataGroupingPartitionByCount.java
@@ -4,14 +4,9 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.NumberFormat;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.reducing;
-
-public class BigDataGroupingPartitionBy {
+public class BigDataGroupingPartitionByCount {
 
         record Person(String firstName, String lastName, BigDecimal salary, String state, char gender){}
     public static void main(String[] args) {
@@ -19,10 +14,10 @@ public class BigDataGroupingPartitionBy {
             long start = System.currentTimeMillis();
             var result = Files.lines(Path.of("/Users/MercyBate/Downloads/Hr5m.csv")).parallel()
                     .skip(1)
-                    .limit(10)
+//                    .limit(10)
                     .map(x -> x.split(","))
                     .map(x -> new Person(x[2], x[4], new BigDecimal(x[25]), x[32], x[5].strip().charAt(0)))
-                    .collect(Collectors.partitioningBy(person -> person.gender=='F'));
+                    .collect(Collectors.partitioningBy(person -> person.gender=='F', Collectors.counting()));
             long end = System.currentTimeMillis();
 //            System.out.printf("$%,d.00%n",result);
             System.out.println(result);
